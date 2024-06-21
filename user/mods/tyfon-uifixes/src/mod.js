738c257cf6203fed8d5fe7784951ff8b2ea91a3b
Blacksheep-25 <5268409+blacksheep25@users.noreply.github.com>
2024-06-21 12:04:11 +1000

    Config Changes

--- user/mods/tyfon-uifixes/src/mod.js
+++ user/mods/tyfon-uifixes/src/mod.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Money_1 = require("C:/snapshot/project/obj/models/enums/Money");
 class UIFixes {
     databaseServer;
     logger;
@@ -19,7 +20,7 @@ class UIFixes {
                 }
                 return original.call(inventoryController, playerData, request, sessionID);
             };
-        }, { frequency: "Once" });
+        }, { frequency: "Always" });
         // Keep quickbinds for items that aren't actually lost on death
         container.afterResolution("InRaidHelper", (_, inRaidHelper) => {
             const original = inRaidHelper.deleteInventory;
@@ -35,7 +36,17 @@ class UIFixes {
                     }
                 }
             };
-        }, { frequency: "Once" });
+        }, { frequency: "Always" });
+        // Handle barter sort type (fixed in 3.9.0)
+        container.afterResolution("RagfairSortHelper", (_, ragfairSortHelper) => {
+            const original = ragfairSortHelper.sortOffers;
+            ragfairSortHelper.sortOffers = (offers, type, direction) => {
+                if (+type == 2) {
+                    offers.sort(this.sortOffersByBarter);
+                }
+                return original.call(ragfairSortHelper, offers, type, direction);
+            };
+        }, { frequency: "Always" });
         staticRouterModService.registerStaticRouter("UIFixesRoutes", [
             {
                 url: "/uifixes/assortUnlocks",
@@ -69,6 +80,12 @@ class UIFixes {
             }
         }
         return result;
+    }
+    sortOffersByBarter(a, b) {
+        const moneyTpls = Object.values(Money_1.Money);
+        const aIsOnlyMoney = a.requirements.length == 1 && moneyTpls.includes(a.requirements[0]._tpl) ? 1 : 0;
+        const bIsOnlyMoney = b.requirements.length == 1 && moneyTpls.includes(b.requirements[0]._tpl) ? 1 : 0;
+        return aIsOnlyMoney - bIsOnlyMoney;
     }
 }
 module.exports = { mod: new UIFixes() };
