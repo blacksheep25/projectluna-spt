109ff7d7573e72d7aa2ce8514d0042b10c71b589
Blacksheep-25 <5268409+blacksheep25@users.noreply.github.com>
2024-06-27 07:47:57 +1000

    Config changes

--- user/mods/acidphantasm-scorpion/src/mod.js
+++ user/mods/acidphantasm-scorpion/src/mod.js
@@ -1,22 +1,65 @@
 "use strict";
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/brace-style */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable @typescript-eslint/brace-style */
+/*
+* If you are reading this, I hope you are enjoying Scorpion
+*
+*
+* I have worked on this mod for several months and have tried my best to make it as easy to read and clean as possible
+* I may not always do things in the best way, but I do try!
+* If you have any questions please reach out to me in the SPT Discord - do not DM me
+*
+*/
 const tsyringe_1 = require("C:/snapshot/project/node_modules/tsyringe");
 const ConfigTypes_1 = require("C:/snapshot/project/obj/models/enums/ConfigTypes");
 const jsonc_1 = require("C:/snapshot/project/node_modules/jsonc");
 const node_fs_1 = __importDefault(require("node:fs"));
 const node_path_1 = __importDefault(require("node:path"));
-// New trader settings
+// Custom Imports
 const traderHelpers_1 = require("./traderHelpers");
 const fluentTraderAssortCreator_1 = require("./fluentTraderAssortCreator");
 const Traders_1 = require("C:/snapshot/project/obj/models/enums/Traders");
 const baseJson = require("../db/base.json");
 const questJson = require("../db/questassort.json");
 const assortJson = require("../db/assort.json");
+const productionJson = require("../db/production.json");
+const weaponCompatibility = require("../config/ModdedWeaponCompatibility.json");
 let realismDetected;
+const loadMessage = {
+    0: "Scorpion has brought his crew into Tarkov",
+    1: "One of us..one of us..one of us",
+    2: "Welcome to the team, you're one of us meow ♡",
+    3: "Call Kenny Loggins because you're in the danger zone",
+    4: "Can I offer you a nice egg in this trying time?",
+    5: "Good news everyone! We have over 100 quests!",
+    6: "Never half-ass two things. Whole-ass one thing.",
+    7: "Thanks for signing up for Cat Facts! You will now receive fun daily facts about CATS!",
+    8: "Thanks for signing up for Dog Facts! You will now receive fun daily facts about DOGS!",
+    9: "A big ball of wibbly wobbly, timey wimey stuff",
+    10: "(╯°□°)╯︵ ┻━┻ ",
+    11: "┬─┬ノ( º _ ºノ)",
+    12: "Treat others how you want to be treated",
+    13: "No act of kindness, no matter how small, is ever wasted",
+    14: "Reticulating Splines...",
+    15: "Unfolding Foldy Chairs...",
+    16: "Pressurizing Fruit Punch Barrel Hydraulics...",
+    17: "Fabricating Imaginary Infrastructure...",
+    18: "We apologize again for the fault in the subtitles. Those responsible for sacking the people who have just been sacked, have been sacked.",
+    19: "Are you suggesting coconuts migrate?",
+    20: "We are now the knights who say ekki-ekki-ekki-pitang-zoom-boing!",
+    21: "Knight jumps queen! Bishop jumps queen! Pawns jump queen!",
+    22: "Hello. My name is Inigo Montoya. You killed my father. Prepare to die.",
+    23: "I spent the last few years building up an immunity to iocane powder.",
+    24: "Rodents Of Unusual Size? I don't think they exist.",
+    25: "Always try to be nice, but never fail to be kind",
+    26: "Never be cruel, never be cowardly",
+    27: "Who do I need to ban? (◣_◢)",
+    28: "This loading message is sponsored by Raid: Shadow Legends"
+};
 class Scorpion {
     mod;
     logger;
@@ -24,12 +67,18 @@ class Scorpion {
     fluentAssortCreator;
     static vfs = tsyringe_1.container.resolve("VFS");
     static config = jsonc_1.jsonc.parse(Scorpion.vfs.readFile(node_path_1.default.resolve(__dirname, "../config/config.jsonc")));
+    // Set the name of mod for logging purposes
     constructor() {
-        this.mod = "acidphantasm-scorpion"; // Set name of mod so we can log it to console later
+        this.mod = "acidphantasm-scorpion";
     }
-    /**
-     * Some work needs to be done prior to SPT code being loaded, registering the profile image + setting trader update time inside the trader config json
-     * @param container Dependency container
+    /*
+     * Some work needs to be done prior to SPT code being loaded
+     *
+     * TLDR:
+     * Resolve SPT Types
+     * Set trader refresh, config, image, flea settings
+     * Register Dynamic Router for Randomization Config
+     *
      */
     preAkiLoad(container) {
         // Get a logger
@@ -91,9 +140,16 @@ class Scorpion {
             }
         ], "aki");
     }
-    /**
-     * Majority of trader-related work occurs after the aki database has been loaded but prior to SPT code being run
-     * @param container Dependency container
+    /*
+     * Some work needs to be done after loading SPT code
+     *
+     * TLDR:
+     * Resolve SPT Types
+     * Add Modded Weapons to Quests
+     * Mod Detection to enable/disable Assort Configuration options
+     * Apply Assort Configurations
+     * Add trader to dictionary, locales, and assort
+     *
      */
     postDBLoad(container) {
         const start = performance.now();
@@ -105,8 +161,12 @@ class Scorpion {
         const assortPriceTable = assortJson.barter_scheme;
         const assortItemTable = assortJson.items;
         const assortLoyaltyTable = assortJson.loyal_level_items;
+        //Run Modded Weapon Compatibility
+        this.moddedWeaponCompatibility();
         //Check Mod Compatibility
         this.modDetection();
+        //Push Production Schemes
+        this.pushProductionUnlocks();
         //Update Assort
         if (Scorpion.config.priceMultiplier !== 1) {
             this.setPriceMultiplier(assortPriceTable);
@@ -139,13 +199,21 @@ class Scorpion {
         this.logger.debug(`[${this.mod}] loaded... `);
         const timeTaken = performance.now() - start;
         if (Scorpion.config.debugLogging) {
-            logger.log(`[${this.mod}] Trader load took ${timeTaken.toFixed(3)}ms.`, "green");
+            logger.log(`[${this.mod}] Trader load took ${timeTaken.toFixed(3)}ms.`, "cyan");
         }
+        logger.log(`[${this.mod}] ${this.getRandomLoadMessage()}`, "cyan");
     }
+    /*
+     *
+     * All functions are below this comment
+     *
+     * Most of these functions should be self explanatory
+     *
+     */
     setRealismDetection(i) {
         realismDetected = i;
         if (realismDetected && Scorpion.config.randomizeBuyRestriction || realismDetected && Scorpion.config.randomizeStockAvailable) {
-            this.logger.log(`[${this.mod}] SPT-Realism detected, disabling randomizeBuyRestriction and/or randomizeStockAvailable:`, "yellow");
+            this.logger.log(`[${this.mod}] SPT-Realism detected, disabling randomizeBuyRestriction and/or randomizeStockAvailable:`, "cyan");
         }
     }
     setPriceMultiplier(assortPriceTable) {
@@ -250,9 +318,7 @@ class Scorpion {
         }
     }
     modDetection() {
-        // Get SPT classes
         const preAkiModLoader = tsyringe_1.container.resolve("PreAkiModLoader");
-        //Mod Compatibility & Base Detection Variable Resolution
         const vcqlCheck = preAkiModLoader.getImportedModsNames().includes("Virtual's Custom Quest Loader");
         const realismCheck = preAkiModLoader.getImportedModsNames().includes("SPT-Realism");
         const vcqlDllPath = node_path_1.default.resolve(__dirname, "../../../../BepInEx/plugins/VCQLQuestZones.dll");
@@ -275,13 +341,109 @@ class Scorpion {
         if (!vcqlCheck) {
             this.logger.error(`[${this.mod}] VCQL not detected. Install VCQL and re-install ${this.mod}.`);
         }
-        // If we need to check for Realism
+        // This is completely unneccessary and I'll fix it, eventually - probably
         if (Scorpion.config.randomizeBuyRestriction || Scorpion.config.randomizeStockAvailable) {
             this.setRealismDetection(realismCheck);
         }
         else {
             this.setRealismDetection(realismCheck);
         }
+    }
+    moddedWeaponCompatibility() {
+        const databaseServer = tsyringe_1.container.resolve("DatabaseServer");
+        const questTable = databaseServer.getTables().templates.quests;
+        const quests = Object.values(questTable);
+        let questType;
+        let weaponType;
+        let wasAdded;
+        if (weaponCompatibility.AssaultRifles.length >= 1) {
+            weaponType = weaponCompatibility.AssaultRifles;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_1_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.SubmachineGuns.length >= 1) {
+            weaponType = weaponCompatibility.SubmachineGuns;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_2_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.Snipers.length >= 1) {
+            weaponType = weaponCompatibility.Snipers;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_3_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.Marksman.length >= 1) {
+            weaponType = weaponCompatibility.Marksman;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_4_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.Shotguns.length >= 1) {
+            weaponType = weaponCompatibility.Shotguns;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_5_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.Pistols.length >= 1) {
+            weaponType = weaponCompatibility.Pistols;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_6_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.LargeMachineGuns.length >= 1) {
+            weaponType = weaponCompatibility.LargeMachineGuns;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_7_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.Carbines.length >= 1) {
+            weaponType = weaponCompatibility.Carbines;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_8_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.Melee.length >= 1) {
+            weaponType = weaponCompatibility.Melee;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_9_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (weaponCompatibility.Explosives.length >= 1) {
+            weaponType = weaponCompatibility.Explosives;
+            questType = quests.filter(x => x._id.includes("Scorpion_10_10_"));
+            wasAdded = true;
+            this.moddedWeaponPushToArray(questType, weaponType);
+        }
+        if (wasAdded) {
+            this.logger.log(`[${this.mod}] Custom Weapons added to proficiency quests. Enjoy!`, "cyan");
+        }
+    }
+    moddedWeaponPushToArray(questType, weaponType) {
+        for (const quest in questType) {
+            for (const condition in questType[quest].conditions.AvailableForFinish) {
+                for (const item in questType[quest].conditions.AvailableForFinish[condition].counter.conditions) {
+                    for (const id of weaponType) {
+                        questType[quest].conditions.AvailableForFinish[condition].counter.conditions[item].weapon.push(id);
+                    }
+                }
+            }
+            if (Scorpion.config.debugLogging) {
+                this.logger.log(`[${this.mod}] ${questType[quest].QuestName} --- Added ${weaponType}`, "cyan");
+            }
+        }
+    }
+    pushProductionUnlocks() {
+        const databaseServer = tsyringe_1.container.resolve("DatabaseServer");
+        const productionTable = databaseServer.getTables().hideout.production;
+        for (const item of productionJson) {
+            productionTable.push(item);
+        }
+    }
+    getRandomLoadMessage() {
+        const value = loadMessage[Math.floor(Math.random() * Object.keys(loadMessage).length)];
+        return value;
     }
 }
 module.exports = { mod: new Scorpion() };
